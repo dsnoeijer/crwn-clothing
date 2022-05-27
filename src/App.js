ac41aa64ef95804f877d2b413f3d2528a7e7a041
WD Snoeijer <88512452+dsnoeijer@users.noreply.github.com>
2022-05-27 23:36:00 +0200

    added browser-router-dom

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
-import Directory from "./Components/Directory/Directory";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Home from "./Routes/Home/Home";
 
-const categories = [
-  {
-    "id": 1,
-    "title": "hats",
-    "imageUrl":
-      "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    "id": 2,
-    "title": "jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    "id": 3,
-    "title": "sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    "id": 4,
-    "title": "womens",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    "id": 5,
-    "title": "mens",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-  }
-]
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the nav bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
 
 const App = () => {
   return (
-    <Directory categories={categories} />
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
   )
 }
 
